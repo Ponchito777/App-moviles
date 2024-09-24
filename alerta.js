3f5fd0bc39c2c5a8f2c385f030d40dafc79bc73b
HeNew4 <angelmaclovio@yahoo.com>
2024-09-23 19:14:43 -0600

    Inicio

---
+++ alerta.js
@@ -0,0 +1,38 @@
+alert("ola");
+//18/09/24 Vázquez Silva Fabián Alonso
+//Comentario de una linea
+/*
+    Comentario 
+    de
+    varias lineas
+*/
+
+//Variables
+/*
+    Las variables guardan información que 
+    puedes reutilizar
+*/
+
+//Declaración de variables
+
+let nombre ="Román";
+const PI = 3.1416;
+var edad = 25;
+
+/*Tipos de datos:
+    Cadena de texto(string): "Hola",'mundo'
+    Tipo numerico(number):5,3,14
+    Booleano(boolean): true,false
+*/
+//Ejemplo
+let mensaje = "Hola Mundo";
+let numero = 42;
+let Estudiante = true;
+
+//Operadores matematicos
+let a = 5;
+let b = 2;
+let suma = a+b;
+let resta = a-b;
+let multiplicacion = a*b;
+let division = a/b;
