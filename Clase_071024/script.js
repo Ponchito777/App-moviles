13ed7d85b6cbece6ad9854efae425948714e84b7
Ponchito777 <vazquezsilvafabianalonso@gmail.com>
2024-10-09 23:03:52 -0600

    "    "

--- Clase_071024/script.js
+++ Clase_071024/script.js
@@ -1,5 +1,5 @@
 const display = document.getElementById('display');
-//Añade al display el valor ingresado por el boton correspondiente
+//Añade al display el valor ingresado por el boton correspondiente añadiendole un valor al valor del display que esta vacio
 function appendToDisplay(value) {
     display.value += value;
 }
@@ -9,8 +9,8 @@ function clearDisplay() {
 }
 //Elimina el ultimo valor ingresado en el display
 function deleteLast() {
-    let displayCopyValue = display.value;
-    display.value = displayCopyValue.slice(0, -1);
+    let displayCopyValue = display.value; //variable para guardar el ultimo valor
+    display.value = displayCopyValue.slice(0, -1); //
 }
 //Calcula el resultado de la operación ingresada
 function calculateResult() {
